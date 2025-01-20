51a6f919cda80c93a1dc052c9dcdbee5f1b291ab
Adam Dinan <adam1989ie@gmail.com>
2025-01-20 20:43:31 +0000

    Remove shell scripts

--- get_tree.sh
+++
@@ -1,1 +0,0 @@
-tree -I '__pycache__|venv'
--- venv_commands.sh
+++
@@ -1,9 +0,0 @@
-
-# create venv
-python3.10 -m venv venv
-
-
-# install dependencies
-source venv/bin/activate
-pip install pandas torch PyYAML
-pip install -e .
