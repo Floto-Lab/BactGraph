fb87742d16ba05bb7e8006d0de6a13d01669d182
macwiatrak <macwiatrak@gmail.com>
2025-02-02 19:31:05 +0000

    feat: change the train_size

--- bactgraph/modeling/train.py
+++ bactgraph/modeling/train.py
@@ -60,7 +60,7 @@ class TrainArgumentParser(Tap):
 
     input_dir: str = "/Users/maciejwiatrak/Downloads/data/"
     output_dir: str = "/tmp/gnn-output/"
-    train_size: float = 0.7
+    train_size: float = 0.6
     test_size: float = 0.2
     random_state: int = 42
     test: bool = False
