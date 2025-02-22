3336be6358439681a4af9365c3a9bd9ed9647d00
macwiatrak <macwiatrak@gmail.com>
2025-02-22 00:29:56 +0000

    feat: revert to using graph only

--- bactgraph/modeling/model.py
+++ bactgraph/modeling/model.py
@@ -128,12 +128,12 @@ class BactGraphModel(pl.LightningModule):
             num_heads=config["num_heads"],
         )
 
-        self.bias = torch.nn.Parameter(torch.zeros(config["n_genes"]), requires_grad=True).unsqueeze(1)
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(config["dropout"])
-        self.gene_matrix = nn.Parameter(
-            nn.init.xavier_normal_(torch.empty(config["n_genes"], config["output_dim"])), requires_grad=True
-        )
+        self.bias = torch.nn.Parameter(torch.zeros(config["n_genes"]), requires_grad=True)  # .unsqueeze(1)
+        # self.relu = nn.ReLU()
+        # self.dropout = nn.Dropout(config["dropout"])
+        # self.gene_matrix = nn.Parameter(
+        #     nn.init.xavier_normal_(torch.empty(config["n_genes"], config["output_dim"])), requires_grad=True
+        # )
 
         # self.gene_layers = nn.ModuleList([nn.Linear(config["output_dim"], 1) for _ in range(config["n_genes"])])
 
@@ -144,13 +144,13 @@ class BactGraphModel(pl.LightningModule):
     def forward(self, x_batch: torch.Tensor, edge_index_batch: torch.Tensor, gene_indices: torch.Tensor):
         """Expects a PyG data object with data.x (node features) and data.edge_index (graph connectivity)."""
         x, edge_index, _ = batch_into_single_graph(x_batch, edge_index_batch.type(torch.long))
-        # batch_size = x_batch.shape[0]
-        # logits = self.gat_module(x, edge_index).squeeze() + self.bias.repeat(batch_size)
-        last_hidden_state = self.gat_module(x, edge_index)
-        last_hidden_state = group_by_label(self.dropout(last_hidden_state), gene_indices.view(-1))
-        logits = torch.einsum(
-            "bnm,bm->bn", last_hidden_state, self.gene_matrix.to(last_hidden_state.device)
-        ) + self.bias.to(last_hidden_state.device)
+        batch_size = x_batch.shape[0]
+        logits = self.gat_module(x, edge_index).squeeze() + self.bias.repeat(batch_size)
+        # last_hidden_state = self.gat_module(x, edge_index)
+        # last_hidden_state = group_by_label(self.dropout(last_hidden_state), gene_indices.view(-1))
+        # logits = torch.einsum(
+        #     "bnm,bm->bn", last_hidden_state, self.gene_matrix.to(last_hidden_state.device)
+        # ) + self.bias.to(last_hidden_state.device)
         # logits = []
         # for idx, gene_lhs in enumerate(last_hidden_state):
         #     logits.append(self.gene_layers[idx](gene_lhs))
@@ -163,7 +163,7 @@ class BactGraphModel(pl.LightningModule):
         x_batch, edge_index_batch, y, gene_indices = batch
         preds = self.forward(x_batch, edge_index_batch.type(torch.long), gene_indices)
 
-        y = group_by_label(y.view(-1).unsqueeze(-1), gene_indices.view(-1))
+        # y = group_by_label(y.view(-1).unsqueeze(-1), gene_indices.view(-1))
         preds = preds.view(-1)
         y = y.view(-1)
         preds = preds[y.view(-1) != -100.0]
@@ -177,7 +177,7 @@ class BactGraphModel(pl.LightningModule):
         x_batch, edge_index_batch, y, gene_indices = batch
         preds = self.forward(x_batch, edge_index_batch.type(torch.long), gene_indices)
 
-        y = group_by_label(y.view(-1).unsqueeze(-1), gene_indices.view(-1))
+        # y = group_by_label(y.view(-1).unsqueeze(-1), gene_indices.view(-1))
         preds = preds.view(-1)
         y = y.view(-1)
         preds = preds[y.view(-1) != -100.0]
