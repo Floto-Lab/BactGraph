062ad75647e1f22c832ca5a5525ec47421e9b561
macwiatrak <macwiatrak@gmail.com>
2025-02-24 09:41:21 +0000

    feat: add fix for isnan

--- bactgraph/modeling/model.py
+++ bactgraph/modeling/model.py
@@ -248,6 +248,13 @@ class BactGraphModel(pl.LightningModule):
                 continue
             pearson_gene = pearson_corrcoef(preds_gene, y_gene)
             r2_gene = r2_score(preds_gene, y_gene)
+
+            if torch.isnan(pearson_gene):
+                continue
+            if torch.isnan(r2_gene):
+                continue
+            pearson_arr.append(pearson_gene)
+            r2_arr.append(r2_gene)
             pearson_arr.append(pearson_gene)
             r2_arr.append(r2_gene)
 
